22429a5dfc39d0e2fcfedf1092ffe32b7f6905d3
bsgao <brandongao2003@gmail.com>
2024-02-13 14:58:15 -0500

    done

--- analyze.py
+++ analyze.py
@@ -1,1 +1,21 @@
 # Place code below to do the analysis part of the assignment.
+import csv
+dic ={}
+with open('/Users/brandongao/hw2/data/clean_data.csv', mode='r') as file:
+    reader = csv.reader(file)
+    next(reader, None)
+
+    for row in reader:
+        year = int(row[0])
+        anomaly = float(row[1])
+        decade = (year // 10) * 10
+        if decade in dic:
+            dic[decade]['total'] += anomaly
+            dic[decade]['count'] += 1
+        else:
+            dic[decade] = {'total': anomaly, 'count': 1}
+
+
+for decade, data in sorted(dic.items()):
+    average_anomaly = data['total'] / data['count']
+    print(f"From Decade {decade} to {decade+9}: Average Temperature Anomaly = {average_anomaly:.2f}°F")
--- munge.py
+++ munge.py
@@ -11,12 +11,21 @@ with open("/Users/brandongao/hw2/data/output.txt","w") as w:
             count+=1
         try:
             int(line.strip("\n").split(" ")[0])
-            w.write(line)
+            parts = line.strip("\n").split()
+                
+            if "***" not in parts and "****" not in parts:
+                processed_line = [parts[0]] + \
+                                     [f"{float(value)/100/1.8:.2f}" 
+                                      for i, value in enumerate(parts[1:-1], 1)] + \
+                                     [parts[-1]]
+                w.write(','.join(processed_line) + '\n')
+
         except:
             continue
+        
+with open("/Users/brandongao/hw2/data/output.txt", "r") as ro:
+    output_lines = ro.readlines()
 
-""" with open("output.txt","w") as wt:
-    for line in wt.readlines():
-        lst = line.strip("\n").split(" ")
-        if "***" or "****" not in lst:
-            wt.write(line) """
+with open("/Users/brandongao/hw2/data/clean_data.csv", "w") as wo:
+    for i in output_lines:
+        wo.write(i)
